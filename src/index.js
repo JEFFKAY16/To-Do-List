fdadcc87754501ab0c0f2d5d4c453e19c67c4de7
Jeff Kayombo <jeffkayombo16@gmail.com>
2021-10-30 21:23:34 +0200

    add interactivity to list items

--- src/index.js
+++ src/index.js
@@ -1,35 +1,6 @@
 import './styles.css';
+import List from './list.js';
 
-const tasks = [
-  {
-    description: 'Play Games',
-    completed: false,
-    index: 0,
-  },
-  {
-    description: 'Study JavaScript',
-    completed: false,
-    index: 1,
-  },
-  {
-    description: 'Sleep',
-    completed: false,
-    index: 2,
-  },
-  {
-    description: 'Hang out with girlfriend',
-    completed: false,
-    index: 3,
-  },
-];
+const todoList = new List();
 
-tasks.forEach((e) => {
-  const template = document.createElement('template');
-  template.innerHTML = `
-  <li>
-    <span class="material-icons show-default">check_box_outline_blank</span>
-    <p contenteditable="true">${e.description}</p>
-    <span class="material-icons more-info">more_vert</span>
-  </li>`;
-  document.querySelector('.items').appendChild(template.content);
-});
+todoList.displayItems();
